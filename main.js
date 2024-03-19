35fb2b873c06d82c17ed9852313e1e8e097dfce3
Ngovan2611 <163293166+Ngovan2611@users.noreply.github.com>
2024-03-19 18:28:16 +0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,27 @@
+var fullName = 'Nay sinh nhật bay hả??';
+var age = 'Thiệt không?';
+var int = 'Hâm';
+
+
+//thông báo
+function showDialog() {
+    alert(fullName)
+}
+showDialog()
+
+alert(age);
+confirm('Thiệt hả!!!!!');
+
+
+console.log(age);
+//
+var myFriend = {
+    name: 'Chi'
+}
+console.log(myFriend)
+function Myclassmate(firstName, lastName, age) {
+    this.firstName;
+    this.lastName;
+    this.age;
+}
+
